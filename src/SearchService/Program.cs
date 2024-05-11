2d600d8a11be4e55166191653056b5e9788a869c
Petar Stojkovski <petars089@gmail.com>
2024-05-11 12:29:54 +0200

    feat(search-service): Implemented logic for initialization of mongo database and seeding data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,5 +15,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitializeDatabase(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 app.Run();
