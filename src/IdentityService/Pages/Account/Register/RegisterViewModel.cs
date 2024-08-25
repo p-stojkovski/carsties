04264aa0e00111cc7506c75a1720ce9639993947
Petar Stojkovski <petars089@gmail.com>
2024-08-25 15:57:00 +0200

    feat(IdentityService): Added a register page

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+        public string ReturnUrl { get; set; }
+        public string Button { get; set; }
+    }
+}
