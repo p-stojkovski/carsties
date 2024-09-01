efcddfaa3c1d13e95d5f227031db6ba5a68fa892
Petar Stojkovski <petars089@gmail.com>
2024-09-01 09:49:37 +0200

    feat(AuctionService): Added consumers for auction finished and bid placed events

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,41 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        private readonly AuctionDbContext _dbContext;
+
+        public BidPlacedConsumer(AuctionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("---> Consuming bid placed.");
+
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if (!ShouldUpdateCurrentHighBid(auction, context.Message))
+            {
+                return;
+            }
+
+            auction.CurrentHighBid = context.Message.Amount;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        private bool ShouldUpdateCurrentHighBid(Auction auction, BidPlaced message)
+        {
+            return auction.CurrentHighBid == null
+                || message.BidStatus.Contains("Accepted")
+                && message.Amount > auction.CurrentHighBid;
+        }
+    }
+}
