df4ccaa35ce60a0cf996799d7bc5675fe47c3543
Petar Stojkovski <petars089@gmail.com>
2024-09-01 10:24:31 +0200

    feat(SearchService): Added consumers for finished auction and placed bid

---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("----> Consuming bid placed: " + context.Message.Id);
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (!ShouldUpdateCurrentHighBid(auction, context.Message))
+        {
+            return;
+        }
+
+        auction.CurrentHighBid = context.Message.Amount;
+
+        await auction.SaveAsync();
+    }
+
+    private bool ShouldUpdateCurrentHighBid(Item auction, BidPlaced message)
+    {
+        return message.BidStatus.Contains("Accepted")
+            && message.Amount > auction.CurrentHighBid;
+    }
+}
