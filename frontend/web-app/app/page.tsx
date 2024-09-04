f770948fed21a9e1f1965575b9019f987c3cc27d
Petar Stojkovski <petars089@gmail.com>
2024-09-04 23:38:54 +0200

    feat(FrontEnd): Created inital nav bar layout

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
+  console.log("Server component")
+
   return (
     <div>
-      <h3 className='text-3xl font-semibold'>
-        Carsties App!
-      </h3>
+      Content goes here
     </div>
   );
 }
