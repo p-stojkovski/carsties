b717c87412b2bb0b4bb904b7429280e8524aa386
Petar Stojkovski <petars089@gmail.com>
2024-09-05 08:56:35 +0200

    feat(FrontEnd): Implemeted initial logic for fetching data from api

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,9 @@
-export default function Home() {
-  console.log("Server component")
+import Listings from "./auctions/Listings";
 
+export default function Home() {
   return (
     <div>
-      Content goes here
+      <Listings />
     </div>
   );
 }
